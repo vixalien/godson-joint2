e191423932cdb04912d63131b9c6451351296439
Angelo Verlain <geoangercola@gmail.com>
2022-07-08 22:44:30 +0200

    add button

---
+++ app/components/button.tsx
@@ -0,0 +1,16 @@
+import { Link } from "@remix-run/react";
+import cn from "classnames";
+
+interface ButtonProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+}
+
+const Button = ({ children, href, className, ...props }: ButtonProps) => {
+  if (!href) {
+    return <a className={cn("button", className)} href={href} {...props}>{children}</a>
+  }
+  return <Link className={cn("button", className)} to={href} {...props}>
+    {children}
+  </Link>
+}
+
+export default Button;
