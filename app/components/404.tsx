8693f0d4ad60e2de0344a5ddd0c4e7b08b2bb30b
Angelo Verlain <geoangercola@gmail.com>
2022-07-09 02:36:59 +0200

    add nice animation on 404 page

--- app/components/404.tsx
+++ app/components/404.tsx
@@ -3,7 +3,11 @@ import Button from "./button"
 export const NotFound = () => {
   return (
     <>
-      <h1 className="404-banner">404</h1>
+      <h1 className="404-banner">
+        <span>4</span>
+        <span>0</span>
+        <span>4</span>
+      </h1>
       <h2>We couldn't find that page</h2>
       <p><Button href="/">Back to Home</Button></p>
     </>
